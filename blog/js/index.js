afff887119abb12dc26b83e30a2b71cdf17d4759
Rohitt Vashishtha <aero31aero@gmail.com>
2017-04-10 01:11:29 +0530

    blog: Add updatePostTitleInHeader()

--- blog/js/index.js
+++ blog/js/index.js
@@ -3,15 +3,17 @@ var main = function(){
 		id: "title-1",
 		title: "How I Wrote My Blog",
 		path: "how-i-wrote-my-blog",
+		date: "27-10-2016",
 	}
 	var post2 = {
 		id: "title-2",
 		title: "Working For Zulip",
 		path: "working-for-zulip",
 	}
-	uiwriter.addPostTitle(post1);
-	uiwriter.addPostTitle(post2);
-	uiwriter.activatePostTitle(post1);
+	interface.addPostTitle(post1);
+	interface.addPostTitle(post2);
+	interface.activatePostTitle(post2);
+	interface.updatePostTitleInHeader(post1);
 }
 
 main();
--- blog/js/interface.js
+++ blog/js/interface.js
@@ -1,23 +1,32 @@
 var interface = function(){
 	var addPostTitle = function(post){
-		var atr; //attribute container
 		var li = document.createElement("li");
 		var list = document.getElementById("post-title-list");
 		li.innerHTML = post.title;
-		atr = document.createAttribute("path");
-		atr.value = post.path;
-		li.setAttributeNode(atr);
-		atr = document.createAttribute("id");
-		atr.value = post.id;
-		li.setAttributeNode(atr);
+		li.setAttribute("path",post.path);
+		li.setAttribute("id",post.id);
 		list.appendChild(li);
 	}
 	var activatePostTitle = function(post){
 		var li = document.getElementById(post.id);
 		li.classList.add("active-title");
 	}
+	var updatePostTitleInHeader = function(post){
+		var date = {};
+		date.year = post.date.split("-")[2];
+		date.month = post.date.split("-")[1];
+		date.day = post.date.split("-")[0];
+		var title = "~/";
+		title += date.year + "/";
+		title += date.month + "/";
+		title += date.day + "/";
+		title += post.path;
+		var titleholder = document.getElementById("post-title");
+		titleholder.innerHTML = title;
+	}
 	return {
 		addPostTitle: addPostTitle,
 		activatePostTitle: activatePostTitle,
+		updatePostTitleInHeader: updatePostTitleInHeader,
 	};
 }();
